979645a2094ca532cef49666646c19b8e4efef92
Zoro <zoro11o500@gmail.com>
2026-02-16 07:39:46 +0530

    Update analyzer.py
    added english titles

--- analyzer.py
+++ analyzer.py
@@ -80,7 +80,8 @@ def get_unique_franchises(username):
 
             media = entry["media"]
             media_id = media["id"]
-            title = media["title"]["romaji"]
+            title_data = media.get("title", {})
+            title = title_data.get("english") or title_data.get("romaji") or "Unknown Title"
 
             duration = media["duration"] or 0
             total_eps = media["episodes"] or 0
@@ -177,3 +178,4 @@ def get_unique_franchises(username):
     franchises.sort(key=lambda x: x["total_minutes"], reverse=True)
 
     return franchises, format_time(total_watch_minutes)
+
