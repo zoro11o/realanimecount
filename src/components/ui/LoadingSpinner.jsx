3d51a58431bf5e52fcb6dba282a22889e6f70079
Zoro <zoro11o500@gmail.com>
2026-05-08 22:17:11 +0530

    Delete src directory

--- src/components/ui/LoadingSpinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner({ message = 'Loading…' }) {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '60vh', color: '#6e7681', fontSize: 15 }}>
-      {message}
-    </div>
-  )
-}
